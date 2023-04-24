2f89eb33415d017b78077fb8b8809d766e018bcd
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-04-24 20:17:16 -0300

    first commit

---
+++ src/main/java/dev/fabriciosilva/webservice/WebserviceApplication.java
@@ -0,0 +1,13 @@
+package dev.fabriciosilva.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceApplication.class, args);
+	}
+
+}
